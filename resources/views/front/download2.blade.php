5bd14ba158472c14a1ccc19e8c4273d6d52d5969
137454534@qq.com <mushroom1117>
2018-06-14 09:58:00 +0800

    修改下载页样式

--- resources/views/front/download2.blade.php
+++ resources/views/front/download2.blade.php
@@ -16,7 +16,6 @@
         </div>
         <div class="col-md-6 col-xs-12 col-sm-12" style="margin-top: 20px">
             <h3 class="Title">支持各主流系统平台使用</h3>
-            <p class="Text">本站提供SS/SSR协议节点，是目前加速最流行的解决方案。支持Windows、Mac、iOS、Android 终端，下载软件添加节点即可使用。如果您是新手用户不清楚如何使用，建议花一点时间查看下面使用教程。</p>
         </div>
     </div>
     <div class="row" style="padding-left: 18%;padding-right: 18%">
@@ -40,7 +39,6 @@
                         <div class="PlanCard__pricing fwb mt-2 c-bw-12">
                             <span class="d-block small fwm mb-4 mb-md-5 mt-2">
                                 <p style="font-size: 10px">系统要求：Android 4.4 及以上</p>
-                                <p>完美支持 穿云梯加速 服务所有特性的客户端，适用于Android。</p>
                             </span>
                         </div>
                         <div class="PlanCard__description">
@@ -71,7 +69,6 @@
                         <div class="PlanCard__pricing fwb mt-2 c-bw-12">
                             <span class="d-block small fwm mb-4 mb-md-5 mt-2">
                                 <p style="font-size: 10px">系统要求：OS X Mavericks 及以上</p>
-                                <p>完美支持 穿云梯加速 服务所有特性的客户端，适用于 Mac</p>
                             </span>
                         </div>
                         <div class="PlanCard__description">
@@ -103,7 +100,6 @@
                         <div class="PlanCard__pricing fwb mt-2 c-bw-12">
                             <span class="d-block small fwm mb-4 mb-md-5 mt-2">
                                 <p style="font-size: 10px">系统要求：Windows XP 及以上</p>
-                                <p>完美支持 穿云梯 服务所有特性的客户端，适用于Windows PC。</p>
                             </span>
                         </div>
                         <div class="PlanCard__description">
@@ -135,7 +131,6 @@
                         <div class="PlanCard__pricing fwb mt-2 c-bw-12">
                             <span class="d-block small fwm mb-4 mb-md-5 mt-2">
                                 <p style="font-size: 10px">系统要求：IOS 8.0以上</p>
-                                <p>完美支持 穿云梯加速 服务所有特性的客户端，适用于IOS。</p>
                             </span>
                         </div>
                         <div class="PlanCard__description">
